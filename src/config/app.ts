ee60fade1c1830b0d2ca971ce62034241b5ddbe3
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-14 20:01:56 +0300

    docs: refactor comments and docs

--- src/config/app.ts
+++ src/config/app.ts
@@ -10,15 +10,6 @@ export default {
    * The configuration setting that determines whether the application is in
    * maintenance mode or not.
    *
-   * @var {boolean} `enabled`: Whether or not the application is in maintenance mode
-   * @var {string[]} `except`: The list of URLs that should not be in maintenance mode
-   * @var {string|null} `redirect`: The URL to redirect to when the application is in maintenance mode
-   * @var {number|null} `refresh`: The refresh configuration for the redirect URL
-   * @var {number|string|null} `retry`: The retry configuration for the redirect URL
-   * @var {string|null} `secret`: The secret to use for the redirect URL
-   * @var {number} `status`: The HTTP status code to use for the redirect URL
-   * @var {string} `template`: The message to display when the application is in maintenance mode
-   *
    */
   maintenanceMode: {
     /**
@@ -45,14 +36,17 @@ export default {
     /**
      * Redirect all incoming requests to absolute or relative URL.
      *
-     * @description This is done by sending the Location HTTP response header that
-     * tells the client to redirect to another URL.
+     * @description This is done by sending the Location HTTP response header
+     * that tells the client to redirect to another URL.
      *
-     * This will override the status code in this configuration to `302 Found` not
-     * `503 Service Unavailable` which means temporarily redirecting to another URL.
+     * This will override the status code in this configuration to `302 Found`
+     * not `503 Service Unavailable` which means temporarily redirecting to
+     * another URL.
      *
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Location
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302
+     * @see
+     * {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Location | MDN HTTP Header Location}
+     * @see
+     * {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/302 | MDN HTTP Status 302}
      *
      * @var {string | null}
      *
@@ -60,16 +54,19 @@ export default {
     redirect: null,
 
     /**
-     * Refresh header that will tell the browser to refresh the page every X seconds.
+     * Refresh header that will tell the browser to refresh the page every X
+     * seconds.
      *
-     * @description This HTTP header is not standard but it's commonly
-     * used on the web and supported in every web browser.
+     * @description This HTTP header is not standard but it's commonly used on
+     * the web and supported in every web browser.
      *
      * This header also accepts a URL value to redirect to after X seconds:
      * `Refresh: 5; url=https://example.com/`.
      *
-     * @see https://stackoverflow.com/questions/283752/refresh-http-header
-     * @see https://en.wikipedia.org/wiki/Meta_refresh
+     * @see
+     * {@link https://stackoverflow.com/questions/283752/refresh-http-header | Stack Overflow - Refresh http header}
+     * @see
+     * {@link https://en.wikipedia.org/wiki/Meta_refresh | wikipedia - Meta Refresh}
      *
      * @example `60`, or `60; url=https://example.com/`
      *
@@ -81,10 +78,11 @@ export default {
     /**
      * Retry-After header that will be sent to the client.
      *
-     * @description Retry-After is a response HTTP header that tells the client how
-     * long he should wait before making a follow-up request.
+     * @description Retry-After is a response HTTP header that tells the client
+     * how long he should wait before making a follow-up request.
      *
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+     * @see
+     * {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After | MDN HTTP Headers RETry-After}
      *
      * @var {number | string | null}
      *
@@ -93,9 +91,9 @@ export default {
 
     /**
      * This a secret key that should be sent within a cookie to access the
-     * application even in maintenance mode. That helps the developers to continue
-     * develop and test the application without the need to turn the maintenance
-     * mode off.
+     * application even in maintenance mode. That helps the developers to
+     * continue develop and test the application without the need to turn the
+     * maintenance mode off.
      *
      * @var {string | null}
      *
@@ -103,14 +101,15 @@ export default {
     secret: null,
 
     /**
-     * HTTP response status code that will be sent within all HTTP responses during
-     * maintenance mode is on.
+     * HTTP response status code that will be sent within all HTTP responses
+     * during maintenance mode is on.
      *
      * The default value is `503 Service Unavailable`.
      *
      * @default 503
      *
-     * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/503
+     * @see
+     * {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/503 | MDN HTTP Status 503}
      *
      * @var number
      *
