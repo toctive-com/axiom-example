9602c0cd13929ac9c8fb6b0f5f995642f875262a
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-15 15:05:17 +0300

    feat: add example routes

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,9 @@
 // import HomeController from '@/App/Controllers/HomeController';
-import { Router } from '@toctive/axiom';
+import { RouteActionParameters, Router } from '@toctive/axiom';
 
-Router.get('/about', () => Math.random().toString(36).substring(7));
+Router.get('/', () => 'Welcome to Axiom');
+Router.get('/random', () => Math.random().toString(36).substring(7));
+Router.get('/echo', ({ request }: RouteActionParameters) => request.toJson());
+Router.get('/echo-as-json', ({ request }: RouteActionParameters) =>
+  JSON.parse(request.toJson()),
+);
