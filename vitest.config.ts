79ae03f1e8609c0438c889e5e9b717cc8974c339
Sameh A. Elalfi <sameh.elalfi.mail@gmail.com>
2023-07-23 19:19:04 +0300

    chore: install vite-tsconfig-paths and supertest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+  plugins: [
+    tsconfigPaths({
+      ignoreConfigErrors: true,
+      parseNative: true,
+      loose: true,
+      root: './',
+    }),
+  ],
+
   test: {
     coverage: {
+      provider: 'v8',
       reporter: ['text', 'json', 'html'],
     },
   },
